9950bf4fd757ce10b01cd1307268a394d7f790aa
Vector2588 <natkamol082@gmail.com>
2021-07-30 00:26:54 +0700

    5.2 Flash message to list

--- question5/src/main.js
+++ question5/src/main.js
@@ -1,10 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import GStore from './store'
-import 'nprogress/nprogress.css'
+import { createApp, reactive } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App)
-    .use(router)
-    .provide('GStore', GStore)
-    .mount('#app')
+// Create a reactive object
+const GStore = reactive({ flashMessage: ''})
+createApp(App).use(store).use(router).provide('GStore', GStore).mount('#app')
--- question5/src/views/Detail/EditPassenger.vue
+++ question5/src/views/Detail/EditPassenger.vue
@@ -1,11 +1,31 @@
 <template>
-    <p>Edit passenger detail here</p>
+  <p>Edit passenger detail here</p>
   <button @click="edit">Edit</button>
 </template>
 
 <script>
 export default {
-    props: ['passenger'],
-    
-}
+  props: ["passenger"],
+  inject: ["GStore"], // <--- Inject the Global Store
+  methods: {
+    edit() {
+      //Assuming successful API call to register them
+
+      //Set a flash message to appear on the next page loaded which says
+      // 'You are succesfully registerd for ' + this.event.title
+
+      this.GStore.flashMessage = "The passenger detail has been updated";
+      setTimeout(() => {
+        //After 3 second remove it
+        this.GStore.flashMessage = "";
+      }, 3000);
+      // Set a flash message to appear on the next page loaded which says
+
+      this.$router.push({
+        name: "Home",
+        params: { id: this.passenger.id },
+      });
+    },
+  },
+};
 </script>
